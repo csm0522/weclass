75172b76224f12ddbc0ed995170fd16f88b4d2b9
Chan <447880522@qq.com>
2016-07-09 20:56:51 +0800

    个人页面补充，个人页面样式修改

--- westu/Application/Home/Controller/personController.class.php
+++ westu/Application/Home/Controller/personController.class.php
@@ -3,6 +3,23 @@ namespace Home\Controller;
 use Think\Controller;
 class personController extends Controller {
     public function index(){
+        $concern = M('concern')->where('uid='.session('uid'))->count();
+        $fans = M('concern')->where('beid='.session('uid'))->count();
+        $this->assign('fan',$fans);
+        $this->assign('concern',$concern);
+        $this->assign('username',session('username'));
+        $intro = M('user')->where('uid='.session('uid'))->getField(intro);
+        $this->assign('intro',$intro);
         $this->display();
+    }
+    public function edit(){
+        $concern = M('concern')->where('uid='.session('uid'))->count();
+        $fans = M('concern')->where('beid='.session('uid'))->count();
+        $this->assign('fan',$fans);
+        $this->assign('concern',$concern);
+        $this->assign('username',session('username'));
+        $intro = M('user')->where('uid='.session('uid'))->getField(intro);
+        $this->assign('intro',$intro);
+        $this->display('editinfo');
     }
 }
