6fce5a4d9d06fe69191d50a9fa1915e0e299d985
Chan <447880522@qq.com>
2016-07-22 11:53:35 +0800

    个人信息编辑页完成，开始进入首页的大改，完成课程详情页的修改。

--- westu/Application/Home/Controller/indexController.class.php
+++ westu/Application/Home/Controller/indexController.class.php
@@ -3,12 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class indexController extends Controller {
     public function index(){
-        if(empty(session('uid'))){
-            
-        }
-        else{
-            $this->assign('name',session('username'));
-        }
+        
         $this->display();
     }
 }
